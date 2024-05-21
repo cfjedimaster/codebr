461a2033e19e2c4823dba10924e85b22fc7fefa0
Raymond Camden <raymondcamden@gmail.com>
2024-05-21 12:48:07 -0500

    we did it, baby!

--- pwa1/app.js
+++ pwa1/app.js
@@ -1,3 +1,25 @@
+/*
+offline joke credit: https://github.com/yesinteractive/dadjokes/blob/master/controllers/jokes.txt
+*/
+let offLineJokes = `What did one pirate say to the other when he beat him at chess?<>Checkmatey.
+I burned 2000 calories today<>I left my food in the oven for too long.
+I startled my next-door neighbor with my new electric power tool. <>I had to calm him down by saying "Don't worry, this is just a drill!"
+I broke my arm in two places. <>My doctor told me to stop going to those places.
+I quit my job at the coffee shop the other day. <>It was just the same old grind over and over.
+I never buy anything that has Velcro with it...<>it's a total rip-off.
+I used to work at a soft drink can crushing company...<>it was soda pressing.
+I wondered why the frisbee kept on getting bigger. <>Then it hit me.
+I was going to tell you a fighting joke...<>but I forgot the punch line.
+What is the most groundbreaking invention of all time? <>The shovel. 
+I'm starting my new job at a restaurant next week. <>I can't wait.
+I visited a weight loss website...<>they told me I have to have cookies disabled.
+Did you hear about the famous Italian chef that recently died? <>He pasta way.
+Broken guitar for sale<>no strings attached.
+I could never be a plumber<>it's too hard watching your life's work go down the drain.
+I cut my finger slicing cheese the other day...<>but I think I may have grater problems than that.
+What time did you go to the dentist yesterday?<>Tooth-hurty.`;
+
+
 document.addEventListener('DOMContentLoaded', init, false);
 
 let $div;
@@ -5,6 +27,9 @@ let $div;
 async function init() {
 	console.log('hello world');
 	$div = document.querySelector('#joke');
+
+  offLineJokes = offLineJokes.replaceAll('<>',' ').split('\n');
+
 	getJoke();
 
 	document.querySelector('#jokeBtn').addEventListener('click', getJoke, false);
@@ -14,13 +39,24 @@ async function init() {
 
 async function getJoke() {
 	$div.innerHTML = '<i>Loading awesomeness...</i>';
-	let req = await fetch('https://icanhazdadjoke.com/', {
-		headers: {
-			'Accept':'application/json'
-		}
-	});
-	let result = await req.json();
-	$div.innerHTML = result.joke;
+  if(navigator.onLine) {
+    let req = await fetch('https://icanhazdadjoke.com/', {
+      headers: {
+        'Accept':'application/json'
+      }
+    });
+    let result = await req.json();
+    $div.innerHTML = result.joke;
+  } else {
+    let joke = offLineJokes[getRandomInt(0, offLineJokes.length)];
+    $div.innerHTML = joke;
+  }
+}
+
+function getRandomInt(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); 
 }
 
 async function registerServiceWorker() {
--- pwa1/sw.js
+++ pwa1/sw.js
@@ -2,7 +2,7 @@ console.log('this is sw.js');
 
 const addResourcesToCache = async (resources) => {
 	console.log('adding stuff to cache');
-  const cache = await caches.open("v2");
+  const cache = await caches.open("v3");
   await cache.addAll(resources);
 };
 
@@ -18,6 +18,14 @@ self.addEventListener("install", (event) => {
   );
 });
 
+const cacheFirst = async (request) => {
+  const responseFromCache = await caches.match(request);
+  if (responseFromCache) {
+    return responseFromCache;
+  }
+  return fetch(request);
+};
+
 self.addEventListener("fetch", (event) => {
-  event.respondWith(caches.match(event.request));
+  event.respondWith(cacheFirst(event.request));
 });
